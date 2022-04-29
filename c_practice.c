c861d0bc78ddb670ffb94dbfd5e32a3b2e3d2dee
issarbhavya <ba1712@srmist.edu.in>
2022-04-29 19:28:29 +0530

    pushing

--- c_practice.c
+++ c_practice.c
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+int main()
+{
+    int n,i=1,c=0;
+    scanf("%d",&n);
+    while(n>i){
+        if (n%i==0){
+            c++;
+                 }
+        i++;
+    }
+    if(c==1){
+        printf("prime");
+    }
+    else{
+        printf("no");
+    }
+    return 0;
+    
+}
